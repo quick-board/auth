3684cf3e2c5fa99c36abbfd8b899562ff6c9dd5c
seungchan <tmdcks3489@gmail.com>
2025-06-26 20:27:43 +0900

    ✨ Add eureka client

--- src/main/java/com/quickboard/auth/AuthApplication.java
+++ src/main/java/com/quickboard/auth/AuthApplication.java
@@ -3,11 +3,13 @@ package com.quickboard.auth;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.TimeZone;
 
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
 
