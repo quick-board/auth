6e2e72f4cb37bb65d7ba3bcb435682d4b7eaf584
seungchan <tmdcks3489@gmail.com>
2025-07-25 19:52:21 +0900

    ➕ Add spring security

--- src/main/java/com/quickboard/auth/account/entity/Account.java
+++ src/main/java/com/quickboard/auth/account/entity/Account.java
@@ -1,17 +1,20 @@
 package com.quickboard.auth.account.entity;
 
 import com.quickboard.auth.account.enums.AccountState;
+import com.quickboard.auth.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.Instant;
+
 @Entity
 @Table(name = "accounts")
 @Getter
 @NoArgsConstructor
-public class Account extends BaseEntity{
+public class Account extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,6 +35,10 @@ public class Account extends BaseEntity{
     @Column(name = "refresh_token")
     @Setter
     private String refreshToken;
+
+    @Column(name = "refresh_expires_at")
+    @Setter
+    private Instant refreshExpiresAt;
 
     @Builder
     public Account(String username, String password, AccountState accountState) {
--- src/main/java/com/quickboard/auth/common/entity/BaseEntity.java
+++ src/main/java/com/quickboard/auth/common/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.quickboard.auth.account.entity;
+package com.quickboard.auth.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
